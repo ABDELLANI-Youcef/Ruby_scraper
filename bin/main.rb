039bfcdf362fbf3cd0f7fda976bcd7bda0be9d97
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2020-09-07 23:54:35 +0100

    be able to extract owner of repository

--- bin/main.rb
+++ bin/main.rb
@@ -10,8 +10,48 @@ doc = Nokogiri::HTML(page)
 # 
 # element.css('svg').remove
 element = doc.css("article")
-element.each do |trend|
+# example = "youcef"
+# puts example[0]
+
+element.each_with_index do |trend,index|
   trend.css('float-right').remove
+  # paragraph of descroption
   description = trend.css('p').text
-  print description
+  
+  unless description.size == 0
+    description2 = ''
+    j = description.size-1
+    while description[j].ord ==32 do
+      j -= 1
+    end
+    j -= 1
+    i=1
+    while (description[i].ord == 32) do
+      i +=1
+    end
+    while i<=j do
+      description2 << description[i]
+      i +=1
+    end
+    print description2
+  end 
+  
+  # .bytes
+  # title
+  title = trend.css("h1")
+  title.css('svg').remove
+  owner = title.css('span').text
+  owner = owner.chomp
+  owner2 = ""
+  owner2 = owner2
+  i=1
+  while (owner[i].ord == 32) do
+    i +=1
+  end
+  while i<owner.size do
+    owner2 << owner[i]
+    i +=1
+  end
+  owner = owner2
+  puts "\n\n" + index.to_s + "\n\n"
 end
